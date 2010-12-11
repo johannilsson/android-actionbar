e097a844980cecac0f338cf5d122784f0266121a
Johan Nilsson <github@markupartist.com>
2010-12-11 16:09:46 +0100

    Removed unused code.

--- actionbarexample/src/com/markupartist/android/actionbar/example/HomeActivity.java
+++ actionbarexample/src/com/markupartist/android/actionbar/example/HomeActivity.java
@@ -1,14 +1,13 @@
 package com.markupartist.android.actionbar.example;
 
 import com.markupartist.android.widget.ActionBar;
-import com.markupartist.android.widget.ActionBar.Action;
+import com.markupartist.android.widget.ActionBar.AbstractAction;
 import com.markupartist.android.widget.ActionBar.IntentAction;
 
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
 
 public class HomeActivity extends Activity {
     /** Called when the activity is first created. */
@@ -34,20 +33,5 @@ public class HomeActivity extends Activity {
         intent.setType("text/plain");
         intent.putExtra(Intent.EXTRA_TEXT, "Shared from the ActionBar widget.");
         return Intent.createChooser(intent, "Share");
-    }
-
-    private class ExampleAction implements Action {
-
-        @Override
-        public int getDrawable() {
-            return R.drawable.ic_title_export_default;
-        }
-
-        @Override
-        public void performAction() {
-            Toast.makeText(HomeActivity.this,
-                    "Example action", Toast.LENGTH_SHORT).show();
-        }
-        
     }
 }
