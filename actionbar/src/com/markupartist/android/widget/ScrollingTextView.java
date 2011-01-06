76fbf95db611eec8e602706c813582c6f77366fa
Johan Nilsson <github@markupartist.com>
2011-01-06 10:34:07 +0100

    Cosmetic fix

--- actionbar/src/com/markupartist/android/widget/ScrollingTextView.java
+++ actionbar/src/com/markupartist/android/widget/ScrollingTextView.java
@@ -16,30 +16,34 @@ import android.widget.TextView;
  */
 public class ScrollingTextView extends TextView {
 
-	public ScrollingTextView(Context context, AttributeSet attrs, int defStyle) {
+	public ScrollingTextView(Context context, AttributeSet attrs,
+	        int defStyle) {
     	super(context, attrs, defStyle);
     }
- 
+
     public ScrollingTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
-    
+
     public ScrollingTextView(Context context) {
         super(context);
     }
-    
+
     @Override
-    protected void onFocusChanged(boolean focused, int direction, Rect previouslyFocusedRect) {
-        if(focused)
+    protected void onFocusChanged(boolean focused, int direction,
+            Rect previouslyFocusedRect) {
+        if (focused) {
             super.onFocusChanged(focused, direction, previouslyFocusedRect);
+        }
     }
- 
+
     @Override
     public void onWindowFocusChanged(boolean focused) {
-        if(focused)
+        if (focused) {
             super.onWindowFocusChanged(focused);
+        }
     }
- 
+
     @Override
     public boolean isFocused() {
         return true;
