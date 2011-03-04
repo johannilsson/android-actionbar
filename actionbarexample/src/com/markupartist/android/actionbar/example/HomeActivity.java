da687d2fc6d32aa88417e942c65be97269f289ca
Johan Nilsson <github@markupartist.com>
2011-03-05 00:09:02 +0100

    Updated the example project to include removeAction and removeActionAt.

--- actionbarexample/src/com/markupartist/android/actionbar/example/HomeActivity.java
+++ actionbarexample/src/com/markupartist/android/actionbar/example/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.markupartist.android.actionbar.example;
 
 import com.markupartist.android.widget.ActionBar;
+import com.markupartist.android.widget.ActionBar.Action;
 import com.markupartist.android.widget.ActionBar.IntentAction;
 
 import android.app.Activity;
@@ -10,6 +11,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class HomeActivity extends Activity {
     /** Called when the activity is first created. */
@@ -21,9 +23,12 @@ public class HomeActivity extends Activity {
         final ActionBar actionBar = (ActionBar) findViewById(R.id.actionbar);
         //actionBar.setHomeAction(new IntentAction(this, createIntent(this), R.drawable.ic_title_home_demo));
         actionBar.setTitle("Home");
-        actionBar.addAction(new IntentAction(this, createShareIntent(), R.drawable.ic_title_share_default));
-        actionBar.addAction(new IntentAction(this, new Intent(this, OtherActivity.class), R.drawable.ic_title_export_default));
-        
+
+        final Action shareAction = new IntentAction(this, createShareIntent(), R.drawable.ic_title_share_default);
+        actionBar.addAction(shareAction);
+        final Action otherAction = new IntentAction(this, new Intent(this, OtherActivity.class), R.drawable.ic_title_export_default);
+        actionBar.addAction(otherAction);
+
         Button startProgress = (Button) findViewById(R.id.start_progress);
         startProgress.setOnClickListener(new OnClickListener() {
             @Override
@@ -48,7 +53,40 @@ public class HomeActivity extends Activity {
             }
         });
 
+        Button addAction = (Button) findViewById(R.id.add_action);
+        addAction.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                actionBar.addAction(new Action() {
+                    @Override
+                    public void performAction(View view) {
+                        Toast.makeText(HomeActivity.this, "Added action.", Toast.LENGTH_SHORT).show();
+                    }
+                    @Override
+                    public int getDrawable() {
+                        return R.drawable.ic_title_share_default;
+                    }
+                });
+            }
+        });
 
+        Button removeAction = (Button) findViewById(R.id.remove_action);
+        removeAction.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int actionCount = actionBar.getActionCount();
+                actionBar.removeActionAt(actionCount - 1);
+                Toast.makeText(HomeActivity.this, "Removed action." , Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button removeShareAction = (Button) findViewById(R.id.remove_share_action);
+        removeShareAction.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                actionBar.removeAction(shareAction);
+            }
+        });
     }
 
     public static Intent createIntent(Context context) {
