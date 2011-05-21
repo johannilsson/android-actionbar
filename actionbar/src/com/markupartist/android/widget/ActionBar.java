b7003878ab8dc98ff653c5003aaf02165975e5c6
Johan Nilsson <github@markupartist.com>
2011-05-22 00:37:03 +0200

    Added Copyright notice for Jake Wharthon.

--- actionbar/src/com/markupartist/android/widget/ActionBar.java
+++ actionbar/src/com/markupartist/android/widget/ActionBar.java
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2010 Johan Nilsson <http://markupartist.com>
- *
+ * Copyright (C) 2011 Jake Wharton <jakewharton@gmail.com>
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
