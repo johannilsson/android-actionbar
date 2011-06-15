02e3ba1f4a268af8fadbd7b5d5762e2695ae6a01
Chris Dzombak <cdzombak@nutshell.com>
2011-06-15 09:38:11 -0400

    Add static final serialVersionUID field to ActionList
    (avoids warning)

--- actionbar/src/com/markupartist/android/widget/ActionBar.java
+++ actionbar/src/com/markupartist/android/widget/ActionBar.java
@@ -246,6 +246,7 @@ public class ActionBar extends RelativeLayout implements OnClickListener {
      * A {@link LinkedList} that holds a list of {@link Action}s.
      */
     public static class ActionList extends LinkedList<Action> {
+        private static final long serialVersionUID = -7639253919045641775L;
     }
 
     /**
