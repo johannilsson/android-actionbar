ede069e8b8050611fdfbd926563978aced257c55
unknown <Alex@.(none)>
2011-08-08 17:43:16 +0100

    Added more Actions, but not yet tested them

---
+++ actionbar/src/com/markupartist/android/widget/OnActionClickListener.java
@@ -0,0 +1,11 @@
+package com.markupartist.android.widget;
+
+public interface OnActionClickListener {
+	
+	/**
+	 * Called when a {@link InterfaceAction} is clicked
+	 * @param a the {@link Action} which was clicked
+	 */
+	public void onActionClick(ActionBar.Action a);
+
+}
